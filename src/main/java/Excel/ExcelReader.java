8595b1ccbb736fd8f12adc30a68ec2d2f2c0ff9d
TokVanShow <ivan.tokun@ya.ru>
2024-04-01 21:45:07 +0300

    Реализация чтения из Excel
    добавлен класс ExcelReader с методами загрузки, чтения и закрытия файла

--- src/main/java/Excel/ExcelReader.java
+++ src/main/java/Excel/ExcelReader.java
@@ -1,7 +1,6 @@
 package Excel;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -12,7 +11,6 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
--- src/main/java/Lab2.java
+++ src/main/java/Lab2.java
@@ -5,7 +5,7 @@ import Excel.ExcelReader;
 public class Lab2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
         ExcelReader excel = new ExcelReader("src\\main\\java\\Excel\\ДЗ4.xlsx");
         excel.run();
     }
